65c90c36fe22e21ecd37ec61c9c80aa40f62361c
EduardoAragon11 <eduardo.aragon@utec.edu.pe>
2025-07-01 00:14:20 -0500

    cmake

--- cmake.py
+++ cmake.py
@@ -8,16 +8,18 @@ if not os.path.isdir(carpeta):
     print(f"La carpeta {carpeta} no existe.")
     exit(1)
 
-subprocess.run('g++ *.cpp -o compiler', shell=True, check=True)
+#subprocess.run('g++ *.cpp -o compiler', shell=True, check=True)
 
 for archivo in os.listdir(carpeta):
     ruta_archivo = os.path.join(carpeta, archivo)
 
     if os.path.isfile(ruta_archivo):
+        print("--------------------------------------------------")
         print(f"Compilando: {ruta_archivo}")
         try:
             subprocess.run(['./compiler', ruta_archivo])
-            subprocess.run(['gcc input.s -o output'])
+            subprocess.run(['gcc', f'{ruta_archivo[:-3]}.s', '-o', 'output'])
             subprocess.run(['./output'])
+            subprocess.run(['rm', f'{ruta_archivo[:-3]}.s'])
         except subprocess.CalledProcessError as e:
             print(f"Error al compilar {ruta_archivo}: {e}")
